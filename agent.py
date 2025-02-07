47335e481f2193a2d658f5d3cbc97c3c90647204
spacholski <spacholski99@gmail.com>
2025-02-07 14:31:18 +0100

    Changed model

--- agent.py
+++ agent.py
@@ -4,7 +4,7 @@ from tools.utils import stretching_video_link
 
 class Agent:
     def ask_agent(question: str):
-        model = LiteLLMModel(model_id="gpt-4o-mini")
+        model = LiteLLMModel(model_id="gpt-4o")
         codeAgent = CodeAgent(tools=[DuckDuckGoSearchTool(), stretching_video_link], model=model)
         answer = codeAgent.run(question)
 
